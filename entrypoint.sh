5fd6862e7ef1b568cccdcfce84921d2b755ea1db
amory <36281195+EggsLeggs@users.noreply.github.com>
2026-05-12 00:45:33 +0100

    fix: chmod /run and socket watcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 chmod -R 755 /app/cache 2>/dev/null || true
 chmod -R 755 /app/data 2>/dev/null || true
+chmod 777 /run 2>/dev/null || true
 
 (
   while true; do
-    chmod 755 /run/granian/ 2>/dev/null || true
+    chmod 777 /run/granian 2>/dev/null || true
     chmod 777 /run/granian/granian.sock 2>/dev/null || true
-    sleep 0.2
+    sleep 0.1
   done
 ) &
 
