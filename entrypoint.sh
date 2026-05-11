b68b87a3279a2b476c614ea1031154108b0f5dc9
amory <36281195+EggsLeggs@users.noreply.github.com>
2026-05-12 00:31:23 +0100

    fix: chmod granian directory and socket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 chmod -R 755 /app/cache 2>/dev/null || true
+chmod -R 755 /app/data 2>/dev/null || true
 
 (
   while true; do
-    if [ -S /run/granian/granian.sock ]; then
-      chmod 777 /run/granian/granian.sock 2>/dev/null
-    fi
-    sleep 0.5
+    chmod 755 /run/granian/ 2>/dev/null || true
+    chmod 777 /run/granian/granian.sock 2>/dev/null || true
+    sleep 0.2
   done
 ) &
 
